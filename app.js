c7ba40f92edcb4330164edf7943ea7bf7e34d1f9
Calixto <calixtoalbea@gmail.com>
2022-05-29 15:47:21 -0300

    Add: Body-Parser

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 const routeReset = require('./routes/reset');
 const routeEvent = require('./routes/event');
 const routeBalance = require('./routes/balance');
+const { application } = require('express');
 
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.json());
 
 app.use('/reset', routeReset)
 app.use('/event', routeEvent);
